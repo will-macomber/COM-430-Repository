b5079c0752e89590eb3dca57429db07a8d44c312
will-macomber <44177708+will-macomber@users.noreply.github.com>
2020-12-20 00:06:12 -0500

    Newest Uploading Function
    I lagged before in uploading files here because I didn't expect my computer to die, and I was the one who was programming the PHP. Josh, I was wondering if you could look through this and let me know if I'm doing something wrong here. I built this, and tested it. I get redirected to another page (that will obviously look the same because we don't have a redirect message yet), but when I refresh the DB, there is nothing in there. I don't get a success or failure message. I'll send you the DB export next time I hear from you.

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
+<?php
+//create the db connection
+//(hostname, db username, db password, db name)
+$db = mysqli_connect("localhost", "root", "", "niceguys");
+
+//initialize the message variable
+$msg = "";
+
+//if the upload button is clicked logic
+if (isset($_POST['submit']))
+{
+    //get the image name
+    $image = $_FILES['image_upload']['name'];
+
+   //get the app name
+   $appName = mysqli_real_escpae_string($db, $_POST['appname']);
+   //get the first name
+   $firstname = mysqli_real_escpae_string($db, $_POST['fn']);
+   //get the last name
+   $lastname = mysqli_real_escpae_string($db, $_POST['ln']);
+
+   //image file directory 
+   $target = "images/".basename($image);
+
+   $sql = "INSERT INTO images (image, firstname, lastname, appname) VALUES ('$image', '$firstname', '$lastname', '$appname')";
+
+   //execute that query up there
+   mysqli_query($db, $sql);
+
+   if(move_uploaded_file($_FILES['image']['tmp_name'], $target))
+   {
+       $msg = "Image uploaded successfully.";
+   }
+   else 
+   {
+       $msg = "Failed to upload image";
+   }
+}
+
+//$result = mysqli_query($db, "SELECT * FROM imges");
+?>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -7,7 +48,7 @@
     <meta name="author" content="Will, Samia, Ben">
 
     <!--Samia and Ben, when you view this, change the href path to your own-->
-    <link rel="stylesheet" href="http://localhost:8080/final_project/CSS/style.css" type="text/css">
+    <link rel="stylesheet" href="http://localhost/Nice Guys/style.css" type="text/css">
 
 
 </head>
@@ -26,12 +67,36 @@
         <h3>Nice Guys</h3>
     </div><!--end header-->
 
+    <!--
+        This portion of code will be where we display the results of
+        searches, but no images have been uploaded, so it throws an error
+        because $results is an empty variable, or undefined if I comment
+        out its definition.
+    -->
+    <!--
+    <div id="content">
+        <?php
+            //while ($row = mysqli_fetch_array($result))
+            //{
+             //   echo "<div id='img_div'>";
+             //       echo "<img src='images/".$row['image']."'>";
+             //       echo "<p>".$row['appname']."</p>";
+             //       echo "<p>".$row['firstname']."</p>";
+             //       echo "<p>".$row['lastname']."</p>";
+             //   echo "</div><!--end img_div -->";
+            //}//end while
+        
+        ?>
+    </div
+    -->
     <div id="forms">
+       <form method="post" action="index.php" enctype="multipart/form-data"> 
         <input type="file" id="image_upload" value="Choose Image"><br>
         <input type="text" placeholder="App Name" required id="appname"><br>
         <input type="text" placeholder="First Name" required id="fn"><br>
-        <input type="text" placeholder="Lastname" required id="ln"><br>
+        <input type="text" placeholder="Last Name" required id="ln"><br>
         <input type="submit" id="submit" value="Submit">
+       </form>
     </div><!--end forms div-->
 
     <div id="id01" class="modal">
